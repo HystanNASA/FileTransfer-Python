c33bbe572bd561c444e3549c954ad137cd6ee0f9
Roman <kenobi-2002@mail.ru>
2019-03-09 22:35:00 +0300

    v1.0

--- main.py
+++ main.py
@@ -1,11 +1,109 @@
 import socket
 import sys
+import argparse
 
 # params :
-# -p    port
-# -h    host
-# -m    msg
+# -s    server      
+# -c    client
+# -p  port
+# -h  host
+# -f    filename    
+
+def serverRoutine(sock, host, port):
+    
+    sock.bind((host, port))
+    sock.listen(1)
+    conn, addr = sock.accept()
+
+    filename = ''
+    while sys.getsizeof(filename) < 255:
+        recvdata = conn.recv(1)
+
+        if recvdata == '.':
+            recvdata += conn.recv(3)
+            filename += recvdata
+            break
+
+        filename += recvdata
+
+    print(filename)
+
+    try:
+        file = open(filename, 'wb')
+
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            file.write(data)
+        
+        file.close()
+    except:    
+        print("Couldn't create the file!")
+
+    return
+
+def clientRoutine(sock, host, port, data, filename):
+
+    try:
+        sock.connect((host, port))
+    except:
+        print("Couldn't connect to server!")
+    
+    try:
+        sock.sendall(filename)
+        sock.sendall(data)
+        print("DONE!")
+    except:
+        print("Couldn't send data to server!")
+
+    return
 
 HOST = socket.gethostname()
 PORT = 17000
-MSG  = 'Hey'
+SERVER = True
+FILE_NAME = ''
+FILE_DATA = ''
+FILE = ''
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", help = "Server", action = "store_true")
+parser.add_argument("-c", help = "Client", action = "store_true")
+parser.add_argument("-p", type = int, help = "Setup the local port")
+parser.add_argument("-ht", type = str, help = "Setup the local host")
+parser.add_argument("-f", type = str, help = "The name of the file")
+args = parser.parse_args()
+
+if args.s:
+    SERVER = True
+
+if args.c:
+    SERVER = False
+
+if args.p:
+    PORT = args.p
+
+if args.ht:
+    HOST = args.ht
+
+if (args.f != None) and (args.c == True):
+    FILE_NAME = args.f
+    try:
+        FILE = open(FILE_NAME, 'rb')
+        FILE_DATA = FILE.read()
+        FILE.close()
+    except:
+        print("Couldn't open the file!")
+        sys.exit(1)
+elif ((args.f == None) and (args.c == True)) or ((args.f != None) and (args.c == False)):
+    print("Use the -f flag if it's client.")
+    sys.exit(1)
+
+s = socket.socket()
+
+if SERVER == True:
+    serverRoutine(s, HOST, PORT)
+else:
+    clientRoutine(s, HOST, PORT, FILE_DATA, FILE_NAME)
+
+s.close()
