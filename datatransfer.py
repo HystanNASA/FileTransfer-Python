2e4c421f9632aeb357c223710f01a948fbcbef4d
Roman <kenobi-2002@mail.ru>
2019-03-10 16:52:50 +0300

    fix bugs

--- datatransfer.py
+++ datatransfer.py
@@ -1,13 +1,14 @@
 import socket
 import sys
 import argparse
+import signal
 
 # params :
 # -s    server      
 # -c    client
 # -p  port
 # -h  host
-# -f    filename    
+# -f    filename
 
 def serverRoutine(sock, host, port):
 
@@ -15,33 +16,19 @@ def serverRoutine(sock, host, port):
     sock.listen(1)
     conn, addr = sock.accept()
 
-    filename = bytes(0)
-    while sys.getsizeof(filename) < 255:
-        recvdata = conn.recv(1)
-
-        if recvdata == '.':
-            recvdata += conn.recv(3)
-            filename += recvdata
-            break
-
-        filename += recvdata
-
-    print(filename)
-
-    try:
-        file = open(filename, 'wb')
+    filename = conn.recv(1024)
+    print filename
+    conn.send('k')
+    
+    with open(filename, 'wb') as file:
 
         while True:
             data = conn.recv(1024)
             if not data:
                 break
             file.write(data)
-        
-        file.close()
 
-        conn.sendall("DONE!")
-    except:    
-        print("Couldn't create the file!")
+        file.close()
 
     return
 
@@ -55,8 +42,9 @@ def clientRoutine(sock, host, port, data, filename):
     
     try:
         sock.sendall(filename)
+        sock.recv(64)
         sock.sendall(data)
-        print(sock.recv(32))
+        print 'DONE!'
     except:
         print("Couldn't send data to server!")
 
@@ -104,9 +92,14 @@ elif ((args.f == None) and (args.c == True)) or ((args.f != None) and (args.c ==
 
 s = socket.socket()
 
-print("Server IP: ", HOST)
+def handler():
+    s.close()
+    return
+
+signal.signal(signal.SIGTSTP, handler)
 
 if SERVER == True:
+    print 'Server IP: ', HOST
     serverRoutine(s, HOST, PORT)
 else:
     clientRoutine(s, HOST, PORT, FILE_DATA, FILE_NAME)
